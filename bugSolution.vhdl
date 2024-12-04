3ccccca1206ae26ebda3de00395ed4da2950a104
BugHunter-X <askzenai@gmail.com>
2024-12-04 20:08:10 +0100

    Add bugSolution.vhdl file

---
+++ bugSolution.vhdl
@@ -0,0 +1,30 @@
+```vhdl
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity correct_counter is
+    port (
+        clk : in std_logic;
+        rst : in std_logic;
+        count : out integer range 0 to 15
+    );
+end entity correct_counter;
+
+architecture behavioral of correct_counter is
+    signal internal_count : integer range 0 to 15 := 0;
+begin
+    process (clk, rst)
+    begin
+        if rst = '1' then
+            internal_count <= 0;
+        elsif rising_edge(clk) then
+            if internal_count = 15 then
+                internal_count <= 0;  
+            else
+                internal_count <= internal_count + 1;
+            end if;
+        end if;
+    end process;
+    count <= internal_count;
+end architecture behavioral;
+```
